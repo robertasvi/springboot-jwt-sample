387e5508ca881b0522cc13541812274032ec27a7
Robert <robertasvilkisius@gmail.com>
2019-11-17 20:08:33 +0200

    Implemented Role service

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -4,4 +4,6 @@ import com.example.demo.domain.Country;
 import com.example.demo.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository  extends JpaRepository<Role, Long> {}
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+    Role findById(long id);
+}
--- src/main/java/com/example/demo/web/RoleController.java
+++ src/main/java/com/example/demo/web/RoleController.java
@@ -2,6 +2,7 @@ package com.example.demo.web;
 
 import com.example.demo.domain.Role;
 import com.example.demo.domain.Type;
+import com.example.demo.domain.User;
 import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +12,35 @@ import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequestMapping("/role")
 public class RoleController {
 
     @Autowired
     RoleRepository roleRepository;
 
-    @RequestMapping(value="/role", method= RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    public Role add(@RequestBody Role role) {
+        return roleRepository.save(role);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public Role update(@RequestBody Role role) {
+        return roleRepository.save(role);
+    }
+
+    @RequestMapping(method= RequestMethod.GET)
     @ResponseBody
     public List<Role> findAll() {
         return roleRepository.findAll();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    @ResponseBody
+    public void delete(@PathVariable long id) {
+        Role existed = this.roleRepository.findById(id);
+        roleRepository.delete(existed);
     }
 
 }
